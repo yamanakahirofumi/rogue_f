12f2bd39b12cad9232b0ec1d57ec7fa2d1e08113
hiro <mokojarasi@gmail.com>
2021-02-15 00:09:29 +0900

    angularのupdateの影響なさげなのでデフォ値変更

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
